73e99b19e5c6b9fd51bca9efe44b3ae2331de636
Kirill Gavrilov <k.gavrilove@gmail.com>
2023-11-03 17:36:31 +0300

    testing

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong.');
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
